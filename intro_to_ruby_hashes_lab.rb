6b3d8fe72879d0298372db1158b55289b23cb335
Yuriy Berezskyy <bereza95@gmail.com>
2019-09-11 03:41:24 +0000

    Automatically backed up by Learn

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -32,9 +32,10 @@ def read_from_hash(hash, key)
 end
 
 def update_counting_hash(hash, key)
- if hash[key] == {}
-   return hash[key] = 1 
- else
-   return hash[key] + 1
- end
+  if hash.key?(key)
+    hash[key] += 1 
+  else
+    hash[key] = 1
+  end
+  hash
 end
